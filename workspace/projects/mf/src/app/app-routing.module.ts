093df9c0e5b94fc85bdd35314fb8034dc0083ebc
DIR\pedro.j.sola.campoy <pedro.j.sola.campoy@accenture.com>
2022-04-06 15:34:17 +0200

    end tutorial

---
+++ workspace/projects/mf/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './default/default.component';
+
+const routes: Routes = [{
+  path:'',
+  component: DefaultComponent
+},
+{
+  path: 'mf',
+  loadChildren: ()=> import('./showinfo/showinfo.module').then((m)=> m.ShowinfoModule )
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
