b6f81f76cadcf68010ebf1542be978dbe2f911fa
DIR\pedro.j.sola.campoy <pedro.j.sola.campoy@accenture.com>
2022-04-08 18:07:18 +0200

    done tutorial

---
+++ ng-mfe/apps/dashboard/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { distinctUntilChanged } from 'rxjs/operators';
+
+import { UserService } from '@ng-mfe/shared/data-access-user';
+
+
+@Component({
+  selector: 'ng-mfe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+
+export class AppComponent implements OnInit {
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit() {
+    this.isLoggedIn$
+      .pipe(distinctUntilChanged())
+      .subscribe(async (loggedIn) => {
+        if (!loggedIn) {
+          this.router.navigateByUrl('login');
+        } else {
+          this.router.navigateByUrl('');
+        }
+      });
+  }
+}
