7d9f7b4c7888d1defb3643c03ef9104cd1a3d0e7
DIR\pedro.j.sola.campoy <pedro.j.sola.campoy@accenture.com>
2022-06-19 23:14:19 +0200

    pre lib

--- workspace/apps/host/src/app/app.module.ts
+++ workspace/apps/host/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterHostComponent } from './footer-host/footer-host.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
     NxWelcomeComponent,
     HomeComponent,
     HeaderComponent,
+    FooterHostComponent,
   ],
   imports: [
     BrowserModule,
---
+++ workspace/apps/host/src/app/footer-host/footer-host.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FooterHostComponent } from './footer-host.component';
+
+describe('FooterHostComponent', () => {
+  let component: FooterHostComponent;
+  let fixture: ComponentFixture<FooterHostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [FooterHostComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(FooterHostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
